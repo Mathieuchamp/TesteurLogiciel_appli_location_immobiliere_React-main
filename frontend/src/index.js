04ef41bc265cd0cd90b5a42264172e0516f07edb
Mathieu champ <champetier.mathieu@orange.fr>
2024-09-02 12:59:11 +0200

    02/09 v2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/location" element={<Location />} />
