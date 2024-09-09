74d10c2213ce0090ed7a07839f9fa484ad8188c3
Mathieu champ <champetier.mathieu@orange.fr>
2024-09-09 20:07:14 +0200

    Dernier

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Notez le changement ici
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
